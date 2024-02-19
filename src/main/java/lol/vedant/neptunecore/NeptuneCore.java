3beb5a736d78d0b7acf518f08becce240efbab0b
VedantMulay <comphackgamer@gmail.com>
2024-02-19 19:13:28 +0530

    Basic Code

---
+++ src/main/java/lol/vedant/neptunecore/NeptuneCore.java
@@ -0,0 +1,16 @@
+package lol.vedant.neptunecore;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class NeptuneCore extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/lol/vedant/neptunecore/managers/StaffChatManager.java
@@ -0,0 +1,2 @@
+package lol.vedant.neptunecore.managers;public class StaffChatManager {
+}
